b1fe3f4cd9a21658bbb63d31e6dc92938ec0c54b
Dildz <roy.madejski.jackson@gmail.com>
2025-03-13 11:36:03 +0200

    updated opening comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-/*********************************************************************************************************
+/*************************************************************************************************************
 * LILYGO T-Display-S3 KY-003 Hall Magnetic Sensor Module Project
 *
 * This code reads the digital signal from the KY-003 module and displays the sensor state on the
-   built-in screen of the LilyGO T-Display-S3 using the TFT_eSPI library.
+*  built-in screen of the LilyGO T-Display-S3 using the TFT_eSPI library.
 * The screen is only updated if there is a change in the sensor state.
-
+*
 * Pin Connections:
 *  - KY-003 VCC  -> 3.3V
 *  - KY-003 GND  -> GND
@@ -16,9 +16,9 @@
 *  - Output: Digital
 *
 * Notes:
-*  - Signal on GPIO1 is active low, this means that when a magnetic field is detected, there is 0V on the pin.
-      If no magnetic field is detected, there is 3.3V on the pin. We start with sensor state HIGH.
-**********************************************************************************************************/
+*  - Signal on GPIO1 is active low, this means that when a magnetic field is detected, there is 0V
+*    on the pin. If no magnetic field is detected, there is 3.3V on the pin. We start with sensor state HIGH.
+*************************************************************************************************************/
 
 /*************************************************************
 ******************* INCLUDES & DEFINITIONS *******************
